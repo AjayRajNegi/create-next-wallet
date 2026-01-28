6c42a9c69d2d5c3d287d02a88ed9ede3c34f4c44
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-01-28 17:26:28 +0530

    Implemented: Mnemonics generation.

--- app/seed/page.tsx
+++ app/seed/page.tsx
@@ -1,3 +1,44 @@
-export default function page() {
-  return <>Seed</>;
+"use client";
+import * as bip39 from "bip39";
+import { useState } from "react";
+
+export default function Page() {
+  const [mnemonic, setMnemonic] = useState<string[]>([]);
+
+  function generateMnemonic() {
+    const str = bip39.generateMnemonic();
+    setMnemonic(str.split(" "));
+    if (mnemonic) {
+      console.log(mnemonic);
+    }
+  }
+
+  return (
+    <>
+      <section className="max-w-7xl mx-auto bg-secondary py-10">
+        <h1 className="text-5xl p-5">Create Solana Wallet</h1>
+        <div className="max-w-6xl mx-auto flex justify-between">
+          <button className="px-4 py-1 text-primary bg-muted-foreground">
+            Add Your Mnemonics +
+          </button>
+          <button
+            className="px-4 py-1 text-primary bg-muted-foreground"
+            onClick={() => {
+              generateMnemonic();
+            }}
+          >
+            Generate +
+          </button>
+        </div>
+
+        <div className="flex gap-4 max-w-6xl mx-auto mt-5">
+          {mnemonic.map((word, id) => (
+            <div className="px-3 py-1 bg-muted-foreground text-white" key={id}>
+              {word}
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
