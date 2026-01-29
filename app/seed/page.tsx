c4a150f49cd6b5022d300208180c61a0d17d3324
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-01-29 12:05:17 +0530

    v1: Can generate pubic and private key pairs.

--- app/seed/page.tsx
+++ app/seed/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Keypair } from "@solana/web3.js";
 import * as bip39 from "bip39";
+import { derivePath } from "ed25519-hd-key";
 import { useState } from "react";
 
 export default function Page() {
   const [mnemonic, setMnemonic] = useState<string[]>([]);
   const [seed, setSeed] = useState<Buffer | null>(null);
+  const [keyPair, setKeyPair] = useState<Keypair | null>(null);
 
   function generateMnemonic() {
     const generatedMnemonic = bip39.generateMnemonic();
@@ -13,6 +16,20 @@ export default function Page() {
     const generatedSeed = bip39.mnemonicToSeedSync(generatedMnemonic, "");
     setSeed(generatedSeed);
     console.log(generatedSeed.toString("hex"));
+  }
+
+  function generateKeyPair() {
+    const path = `m/44'/501'/0'/0'`;
+    if (!seed) {
+      return;
+    }
+    const derivedSeed = derivePath(path, seed.toString("hex")).key;
+    const keyPair = Keypair.fromSeed(derivedSeed);
+
+    setKeyPair(keyPair);
+
+    console.log("Public key:", keyPair.publicKey.toBase58());
+    console.log("Private key:", Buffer.from(keyPair.secretKey).toString("hex"));
   }
 
   return (
@@ -39,6 +56,35 @@ export default function Page() {
               {word}
             </div>
           ))}
+        </div>
+
+        <div className=" gap-4 max-w-6xl mx-auto mt-10">
+          <div
+            className="px-4 py-1 text-accent bg-muted-foreground text-center "
+            onClick={() => {
+              generateKeyPair();
+            }}
+          >
+            Show
+          </div>
+          <div className="bg-background text-foreground p-4 shadow-lg shadow-accent-foreground">
+            <h3>
+              <span className="bg-muted-foreground px-4 py-2 mr-2 text-primary">
+                Public Key <br />
+              </span>
+              {keyPair?.publicKey.toBase58()}
+            </h3>
+            <h3 className="">
+              <span className="bg-muted-foreground px-4 py-2 mr-2 text-primary break-all">
+                Private Key <br />
+              </span>
+              {keyPair?.secretKey ? (
+                <>{Buffer.from(keyPair.secretKey).toString("hex")}</>
+              ) : (
+                <></>
+              )}
+            </h3>
+          </div>
         </div>
       </section>
     </>
